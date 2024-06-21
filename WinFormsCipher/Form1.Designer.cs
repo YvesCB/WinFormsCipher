8e1996d17d28a1932d0b727111506ee804b7eb6c
yves <yves@DESKTOP-KPQVB74>
2024-06-21 20:09:31 +0100

    Add project files.

---
+++ WinFormsCipher/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace WinFormsCipher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.plainInputTextBox = new System.Windows.Forms.TextBox();
+            this.encryptButton = new System.Windows.Forms.Button();
+            this.decryptButton = new System.Windows.Forms.Button();
+            this.keyTextBox = new System.Windows.Forms.TextBox();
+            this.plainTextLabel = new System.Windows.Forms.Label();
+            this.encryptionKeyLabel = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.chipherTextBox = new System.Windows.Forms.TextBox();
+            this.chipherTextLabel = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // plainInputTextBox
+            // 
+            this.plainInputTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.SetColumnSpan(this.plainInputTextBox, 2);
+            this.plainInputTextBox.Location = new System.Drawing.Point(3, 314);
+            this.plainInputTextBox.Multiline = true;
+            this.plainInputTextBox.Name = "plainInputTextBox";
+            this.plainInputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.plainInputTextBox.Size = new System.Drawing.Size(378, 200);
+            this.plainInputTextBox.TabIndex = 0;
+            // 
+            // encryptButton
+            // 
+            this.encryptButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.encryptButton.Location = new System.Drawing.Point(3, 591);
+            this.encryptButton.Name = "encryptButton";
+            this.encryptButton.Size = new System.Drawing.Size(177, 21);
+            this.encryptButton.TabIndex = 1;
+            this.encryptButton.Text = "Encrypt";
+            this.encryptButton.UseVisualStyleBackColor = true;
+            this.encryptButton.Click += new System.EventHandler(this.encryptButton_Click);
+            // 
+            // decryptButton
+            // 
+            this.decryptButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.decryptButton.Location = new System.Drawing.Point(186, 591);
+            this.decryptButton.Name = "decryptButton";
+            this.decryptButton.Size = new System.Drawing.Size(195, 21);
+            this.decryptButton.TabIndex = 2;
+            this.decryptButton.Text = "Decrypt";
+            this.decryptButton.UseVisualStyleBackColor = true;
+            this.decryptButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // keyTextBox
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.keyTextBox, 2);
+            this.keyTextBox.Location = new System.Drawing.Point(3, 540);
+            this.keyTextBox.Multiline = true;
+            this.keyTextBox.Name = "keyTextBox";
+            this.keyTextBox.Size = new System.Drawing.Size(378, 45);
+            this.keyTextBox.TabIndex = 4;
+            // 
+            // plainTextLabel
+            // 
+            this.plainTextLabel.AutoSize = true;
+            this.plainTextLabel.Location = new System.Drawing.Point(3, 293);
+            this.plainTextLabel.Name = "plainTextLabel";
+            this.plainTextLabel.Size = new System.Drawing.Size(54, 13);
+            this.plainTextLabel.TabIndex = 5;
+            this.plainTextLabel.Text = "Plain Text";
+            // 
+            // encryptionKeyLabel
+            // 
+            this.encryptionKeyLabel.AutoSize = true;
+            this.encryptionKeyLabel.Location = new System.Drawing.Point(3, 520);
+            this.encryptionKeyLabel.Name = "encryptionKeyLabel";
+            this.encryptionKeyLabel.Size = new System.Drawing.Size(78, 13);
+            this.encryptionKeyLabel.TabIndex = 6;
+            this.encryptionKeyLabel.Text = "Encryption Key";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 47.53247F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 52.46753F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 182F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 187F));
+            this.tableLayoutPanel1.Controls.Add(this.keyTextBox, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.encryptionKeyLabel, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.plainInputTextBox, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.plainTextLabel, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.encryptButton, 0, 6);
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.chipherTextBox, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.chipherTextLabel, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.decryptButton, 1, 6);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 2, 6);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(31, 23);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 8;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 263F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 61.36364F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 38.63636F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 209F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 17F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 51F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 27F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 13F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(754, 629);
+            this.tableLayoutPanel1.TabIndex = 7;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tableLayoutPanel1.SetColumnSpan(this.dataGridView1, 4);
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(748, 257);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // chipherTextBox
+            // 
+            this.chipherTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.SetColumnSpan(this.chipherTextBox, 2);
+            this.chipherTextBox.Location = new System.Drawing.Point(387, 314);
+            this.chipherTextBox.Multiline = true;
+            this.chipherTextBox.Name = "chipherTextBox";
+            this.chipherTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.chipherTextBox.Size = new System.Drawing.Size(364, 200);
+            this.chipherTextBox.TabIndex = 7;
+            // 
+            // chipherTextLabel
+            // 
+            this.chipherTextLabel.AutoSize = true;
+            this.chipherTextLabel.Location = new System.Drawing.Point(387, 293);
+            this.chipherTextLabel.Name = "chipherTextLabel";
+            this.chipherTextLabel.Size = new System.Drawing.Size(67, 13);
+            this.chipherTextLabel.TabIndex = 8;
+            this.chipherTextLabel.Text = "Chipher Text";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(387, 591);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(176, 21);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(819, 680);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Cipher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox plainInputTextBox;
+        private System.Windows.Forms.Button encryptButton;
+        private System.Windows.Forms.Button decryptButton;
+        private System.Windows.Forms.TextBox keyTextBox;
+        private System.Windows.Forms.Label plainTextLabel;
+        private System.Windows.Forms.Label encryptionKeyLabel;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox chipherTextBox;
+        private System.Windows.Forms.Label chipherTextLabel;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WinFormsCipher/IEncryptDecrypt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsCipher
+{
+    internal interface IEncryptDecrypt
+    {
+        string Encrypt(string plainText, string key);
+        string Decrypt(string cipherText, string key);
+    }
+}
